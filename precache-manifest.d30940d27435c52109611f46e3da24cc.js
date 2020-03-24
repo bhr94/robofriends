ad024e33efd792bfeb854aef302408d4059f0ba4
Bahar94-Mutadayin <bahar.mubtadayin@gmail.com>
2020-03-23 22:21:36 -0700

    Updates

---
+++ precache-manifest.d30940d27435c52109611f46e3da24cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950d584774d4f7a23a8c7830af9395b9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d4dc956eb6df33e9687c",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "60fe239fac1d93b16e1c",
+    "url": "/robofriends/static/css/main.bb79c5b8.chunk.css"
+  },
+  {
+    "revision": "d4dc956eb6df33e9687c",
+    "url": "/robofriends/static/js/2.3a7757fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.3a7757fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60fe239fac1d93b16e1c",
+    "url": "/robofriends/static/js/main.51fd08ba.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  }
+]);
